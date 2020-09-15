b4099cf322e94418a9655874cc57649efaff1098
Thomas Quigley <quigt7936@wrdsb.ca>
2020-09-15 13:06:27 +0000

    Completed the question

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * The program sorts the users two numbers by smallest to largest
+ * @author Thomas Quigley
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+
+    // gets and stores the users two numbers
+    System.out.println("Please enter an integer:");
+    int userIntA = input.nextInt();
+    System.out.println("Please enter another integer:");
+    int userIntB = input.nextInt();
+
+    // determines which number is larger and gives them to the user in order from smallest to largest
+    if (userIntA > userIntB) {
+      System.out.println("Your numbers in ascending order are " + userIntB + ", " + userIntA);
+    } else if (userIntA < userIntB) {
+      System.out.println("Your numbers in ascending order are " + userIntA + ", " + userIntB);
+    }
   }
 }
